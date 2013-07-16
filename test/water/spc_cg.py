faf8591446333d38425b1d648614116d5a73807c
Andrew White <white.d.andrew@gmail.com>
2013-07-15 22:12:19 -0500

    Squashed bugs, made it possible to have a lammps input script. Current issue is forces are attractive instead of repulsive.

--- test/water/spc_cg.py
+++ test/water/spc_cg.py
@@ -4,7 +4,8 @@ import numpy as np
 
 
     
-cgu = CGUniverse(Universe("test/water/spc.tpr", "test/water/traj.trr"), ['name OW', 'name HW1 or name HW2'], ['O', 'H2'], collapse_hydrogens=False)
+#cgu = CGUniverse(Universe("test/water/spc.tpr", "test/water/traj.trr"), ['name OW', 'name HW1 or name HW2'], ['O', 'H2'], collapse_hydrogens=False)
+cgu = CGUniverse(Universe("test/water/spc.tpr", "test/water/traj.trr"), ['all'], ['HOH'], collapse_hydrogens=False)
 cgu.add_residue_bonds("name O", "name H2")
 fm = ForceMatch(cgu, "test/water/spc_cg.json")
 ff = FileForce()
@@ -16,6 +17,6 @@ bwf.add_regularizer(SmoothRegularizer)
 fm.add_ref_force(ff)
 fm.add_and_type_pair(pwf)
 fm.add_and_type_pair(bwf)
-#fm.force_match(1)
-cgu.write_lammps_scripts(fm, folder='lammps', table_points=100)
+fm.force_match(5)
+cgu.write_lammps_scripts(fm, folder='lammps', table_points=1000, lammps_input_file="test/water/lammps.inp")
 #fm.observation_match(obs_sweeps = 3, obs_samples = 5)

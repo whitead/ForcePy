7bd4a75ea2630218346b6d27daafe0614490c34c
Andrew White <white.d.andrew@gmail.com>
2013-08-30 08:30:16 -0500

    Included some functions from CGMap

--- ForcePy/__init__.py
+++ ForcePy/__init__.py
@@ -1,6 +1,6 @@
 from ForceMatch import ForceMatch, Pairwise, Bond
 from Forces import FileForce, AnalyticForce, SpectralForce, SmoothRegularizer, L2Regularizer, LJForce, HarmonicForce, FixedHarmonicForce
 import Mesh
-from CGMap import CGUniverse
+from CGMap import CGUniverse, add_residue_bonds, write_structure, write_trajectory
 import Basis
 

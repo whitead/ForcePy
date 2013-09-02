fb36ef69a4fdb9c492a3f67b5076f09730de8083
Andrew White <white.d.andrew@gmail.com>
2013-09-02 11:37:49 -0500

    Updated lj example

--- test/lj/lj_parallel.py
+++ test/lj/lj_parallel.py
@@ -12,5 +12,4 @@ pwf = SpectralForce(Pairwise, mesh, Basis.Gaussian(mesh, 1))
 pwf.add_regularizer(SmoothRegularizer)
 fm.add_ref_force(ff)
 fm.add_and_type_pair(pwf)
-
 fm.force_match_mpi(5, do_plots=True)

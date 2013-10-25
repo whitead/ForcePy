8ae50f8012ae5b1da6aaa136abf1aad5bf308ade
Andrew White <white.d.andrew@gmail.com>
2013-10-25 11:58:14 -0500

    Lammps force dumps still appear to be the wrong sign

--- ForcePy/CGMap.py
+++ ForcePy/CGMap.py
@@ -343,8 +343,9 @@ class CGReader(base.Reader):
                 pass
             for i in range(len(forces)):
                 sline = self.lfdump.readline().split()
+                #NOTE: Forces from lammps seem to be negative
                 try:
-                    forces[int(sline[0]) - 1,:] = [float(x) for x in sline[1:]]                        
+                    forces[int(sline[0]) - 1,:] = [-float(x) for x in sline[1:]]                        
                 except ValueError:
                     raise IOError( 'Invalid forces line at %s' % reduce(lambda x,y: x + ' ' + y, sline))
             self.ts._forces[:] = self.force_map.dot( forces) 

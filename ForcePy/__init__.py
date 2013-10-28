208bde9963904f77df309c2ffffec7f12bb584df
Andrew White <white.d.andrew@gmail.com>
2013-10-28 02:47:40 -0500

    added build_repulsion to default imports

--- ForcePy/__init__.py
+++ ForcePy/__init__.py
@@ -1,5 +1,5 @@
 from ForcePy.ForceMatch import ForceMatch, Pairwise, Bond
-from ForcePy.Forces import FileForce, AnalyticForce, SpectralForce, SmoothRegularizer, L2Regularizer, LJForce, HarmonicForce, FixedHarmonicForce
+from ForcePy.Forces import FileForce, AnalyticForce, SpectralForce, SmoothRegularizer, L2Regularizer, LJForce, HarmonicForce, FixedHarmonicForce, build_repulsion
 import ForcePy.Mesh as Mesh
 from ForcePy.CGMap import CGUniverse, add_sequential_bonds, add_residue_bonds, write_structure, write_trajectory, write_lammps_data, add_residue_bonds_table
 import ForcePy.Basis

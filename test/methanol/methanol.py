bf63f5df64dae05373bd9666f4866aa623c3f49a
Andrew White <white.d.andrew@gmail.com>
2013-10-29 12:48:44 -0500

    Merged of conflicts and added methanol benchmark code

--- test/methanol/methanol.py
+++ test/methanol/methanol.py
@@ -5,16 +5,30 @@ from ForcePy import *
 fm = ForceMatch(Universe("cg.pdb", "cg.trr"))
 fm.u.trajectory.periodic = True
 ff = FileForce()
-pmesh = Mesh.UniformMesh(2.7,15,0.1)
+pmesh = Mesh.UniformMesh(2,12,0.1)
+#pwf = SpectralForce(Pairwise, pmesh, Basis.UnitStep, build_repulsion(pmesh, 3, 500, 3))
 pwf = SpectralForce(Pairwise, pmesh, Basis.UnitStep)
+#pwf = SpectralForce(Pairwise, pmesh, Basis.Gaussian(pmesh, 0.1))
 #pwf.add_regularizer(L2Regularizer)
 #pwf.add_regularizer(SmoothRegularizer)
 fm.add_ref_force(ff)
 fm.add_and_type_pair(pwf)
 
-#fm.force_match()
+fm.force_match(1)
+fm.finalize()
+fm.write('batch_1', table_points=100)
+fm.finalize()
 
-#this code will show the force_matching in steps of 10
-for i in range(5):
-    fm._force_match_task(10 * i, 10 * (i + 1), do_print=True)
-    fm.write_lammps_scripts(folder='iter_%d' % i, table_points=10000)
+fm.force_match(10)
+fm.finalize()
+fm.write('batch_10', table_points=100)
+fm.finalize()
+
+fm.force_match(100)
+fm.finalize()
+fm.write('batch_100', table_points=100)
+fm.finalize()
+
+fm.force_match()
+fm.finalize()
+fm.write('batch_all', table_points=100)

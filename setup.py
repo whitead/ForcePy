e9088602593f018932dea9bb347f22bb2eb5e07f
Andrew White <white.d.andrew@gmail.com>
2015-06-10 22:56:44 -0400

    Removed gcc specific compile flag

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
             -Wunreachable-code -Werror'
         define_macros = [('DEBUG', '1')]
     else:
-        extra_compile_args = ['-Ofast']
+        extra_compile_args = ['-O3']
         define_macros = []
 
     extensions = [Extension('ForcePy.Util', ['ForcePy/Util.%s' % ("pyx" if use_cython else "c")],
